d41a1921d9b17f0f6b23f9978e663672d91283b2
NoodleSushi <ytmikesstuffs@gmail.com>
2024-10-26 15:50:44 +0800

    git init

---
+++ Calculator/Calculator/CalculatorProcessor.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class CalculatorProcessor
+    {
+        public enum Operation
+        {
+            Nil,
+            Add,
+            Sub,
+            Mul,
+            Div,
+            Err,
+            Equ,
+        }
+
+        private decimal spare = 0;
+        private string input = "0";
+        private bool isDecimal = false;
+        private Operation lastOp = Operation.Nil;
+
+        private void ResetInput()
+        {
+            input = "0";
+            isDecimal = false;
+        }
+
+        public void Clear()
+        {
+            ResetInput();
+            spare = 0;
+            lastOp = Operation.Nil;
+        }
+
+        public string GetDisplay() => input;
+
+        public void InputNumber(int num)
+        {
+            if (lastOp == Operation.Equ)
+                Clear();
+            
+            if (input.Equals("0"))
+                input = num.ToString();
+            else
+                input += num.ToString();
+        }
+
+        public void InputOperation(Operation op)
+        {
+            if (lastOp == Operation.Nil)
+                spare = decimal.Parse(input);
+            else if (lastOp != Operation.Equ)
+                ExecuteOperation();
+
+            ResetInput();
+            lastOp = op;
+        }
+
+        private void ExecuteOperation()
+        {
+            decimal inputParsed = decimal.Parse(input);
+
+            switch (lastOp)
+            {
+                case Operation.Add:
+                    spare += inputParsed;
+                    break;
+                case Operation.Sub:
+                    spare -= inputParsed;
+                    break;
+                case Operation.Mul:
+                    spare *= inputParsed;
+                    break;
+                case Operation.Div:
+                    if (inputParsed == 0)
+                    {
+                        input = "Cannot divide by zero";
+                        lastOp = Operation.Err;
+                    }
+                    else
+                    {
+                        spare /= inputParsed;
+                    }
+                    break;
+            }
+        }
+
+        public void Delete()
+        {
+            if (lastOp == Operation.Equ)
+                return;
+
+            if (input.Substring(input.Length - 1).Equals("."))
+                isDecimal = false;
+
+            input = (input.Length == 1) ? "0" : input.Substring(0, input.Length - 1);
+        }
+
+        public void ToggleDecimal()
+        {
+            if (lastOp == Operation.Equ)
+                return;
+
+            if (isDecimal && input.Substring(input.Length - 1).Equals("."))
+            {
+                isDecimal = false;
+                Delete();
+            }
+            else if (!isDecimal)
+            {
+                isDecimal = true;
+                input += ".";
+            }
+        }
+
+        public void Evaluate()
+        {
+            if (lastOp == Operation.Equ || lastOp == Operation.Nil)
+                return;
+
+            ExecuteOperation();
+
+            if (lastOp != Operation.Err)
+                input = spare.ToString();
+
+            lastOp = Operation.Equ;
+        }
+    }
+}
---
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        private CalculatorProcessor calc = new CalculatorProcessor();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDigitClicked(object sender, EventArgs e)
+        {
+            calc.InputNumber(int.Parse((sender as Button).ClassId));
+            UpdateDisplay();
+        }
+
+        private void OnDelClicked(object sender, EventArgs e)
+        {
+            calc.Delete();
+            UpdateDisplay();
+        }
+
+        private void OnDecClicked(object sender, EventArgs e)
+        {
+            calc.ToggleDecimal();
+            UpdateDisplay();
+        }
+
+        private void OnOpClicked(object sender, EventArgs e)
+        {
+            calc.InputOperation((CalculatorProcessor.Operation) Enum.Parse(typeof(CalculatorProcessor.Operation), (sender as Button).ClassId));
+            UpdateDisplay();
+        }
+
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+            calc.Clear();
+            UpdateDisplay();
+        }
+
+        private void OnEqualsClicked(object sender, EventArgs e)
+        {
+            calc.Evaluate();
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            CalcLabel.Text = calc.GetDisplay();
+        }
+    }
+}
